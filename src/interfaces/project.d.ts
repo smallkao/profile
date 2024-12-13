e306527a077970ff7cb501163175460fc0252adc
TAOLIN\kao0818 <s852468@gmail.com>
2024-12-13 17:58:57 +0800

    定義project interface接口

---
+++ src/interfaces/project.d.ts
@@ -0,0 +1,8 @@
+export  interface ProjectOverview {
+  id: number // 
+  title: string // 專案名稱
+  description: string // 專案描述
+  technologies: string[] // 使用的技術關鍵字
+  websiteUrl?: string // 專案官方網站或預覽網址，可選
+  images?: ImageResource[] // 專案相關圖片
+}
