f5c5bdd624620965e35b52aed345d8aa14407022
TAOLIN\kao0818 <s852468@gmail.com>
2024-12-13 17:57:57 +0800

    專案環境設定

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,59 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite' 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({
+      dirs: ['src/components',],
+      // resolvers: [ElementPlusResolver()],
+      extensions: ['vue'],
+      // 配置文件生成位置
+      dts: 'src/components.d.ts',
+    }),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        'vue-router',
+        'vuex',
+        {
+          // custom
+          '@vueuse/core': [
+            // named imports
+            'useMouse', // import { useMouse } from '@vueuse/core',
+            // alias
+            ['useFetch', 'useMyFetch'],
+          ],
+          axios: [
+            // default imports
+            ['default', 'axios'],
+          ],
+        },
+      ],
+      dts: 'src/types/auto-imports.d.ts', // typescript 宣告檔案位置
+      vueTemplate: false,
+      eslintrc: {
+        enabled: false, // Default `false`
+        filepath: './.eslintrc-auto-import.json',
+        globalsPropValue: true,
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
